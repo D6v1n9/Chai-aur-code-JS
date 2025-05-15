4704150fdef908b0fd5285d595e27ff6c7e444b5
Karan_d6v1n9 <132968913+D6v1n9@users.noreply.github.com>
2025-05-15 16:42:38 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -44,17 +44,17 @@ let str1 = "hello"
 let str2 = " hitesh"
 
 let str3 = str1 + str2
-// console.log(str3);
+// console.log(str3); //hello hitesh
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2);         //12   
+// console.log(1 + "2");         //12
+// console.log("1" + 2 + 2);     //122
+// console.log(1 + 2 + "2");     //32
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true);     //1
+// console.log(+"");       //0
 
 let num1, num2, num3
 
@@ -64,5 +64,5 @@ let gameCounter = 100
 ++gameCounter;
 console.log(gameCounter);
 
-// link to study
+// link to study conversion from ECMA
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
