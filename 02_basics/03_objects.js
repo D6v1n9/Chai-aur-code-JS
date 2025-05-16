bed6adf3fe78068f6eb150d605fea084f50fc205
Karan_d6v1n9 <132968913+D6v1n9@users.noreply.github.com>
2025-05-16 23:59:58 +0530

    Create 03_objects.js

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,39 @@
+// The only difference of creating object by literals or by constructor is of creation of singleton
+// singleton 
+// Object.create -> this method is creating via Constructor and Singleton is created in this 
+
+// object literals -> Singleton is not created in this 
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
