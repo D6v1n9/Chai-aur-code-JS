6358a38250037d85077c7d25c43710b102063b11
Karan_d6v1n9 <132968913+D6v1n9@users.noreply.github.com>
2025-05-17 20:09:22 +0530

    Create 03_arrow.js

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,55 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);            // Inside node this refer to Empty object {} 
+                                // whereas in Browser it refer to Window functions like onclick, alert etc...
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);      // undefined -> this does not work in function
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);      
+}
+
+// chai() // output: {}           //#NOTE this in the arrow function refers to the global object
+
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
