824e0111e5796b4d7b8944ab0cd494d9d3a7587a
Karan_d6v1n9 <132968913+D6v1n9@users.noreply.github.com>
2025-05-18 19:50:28 +0530

    Create 03_for_of_loop.js

---
+++ 05_iterations/03_for_of_loop.js
@@ -0,0 +1,42 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+// Map objects are collections of key-value pairs. A key in the Map may only occur once; it is unique in the Map's collection. 
+// A Map object is iterated by key-value pairs — a for...of loop returns a 2-member array of [key, value] for each iteration. 
+// Iteration happens in insertion order.
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);                // Does not work thus you cannot iterate Object like this
+// }                                                 // so we use for in loop
