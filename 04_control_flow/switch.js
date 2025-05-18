902e3ef2656aee52614eab0ef98ef82136d287d3
Karan_d6v1n9 <132968913+D6v1n9@users.noreply.github.com>
2025-05-18 15:46:16 +0530

    Create switch.js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//#NOTE : break statement not there it will execute all below code except default
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
