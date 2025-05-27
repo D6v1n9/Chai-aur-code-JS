46fe7692919dc56bb704330758947811499bc3fb
Karan_d6v1n9 <132968913+D6v1n9@users.noreply.github.com>
2025-05-27 20:00:46 +0530

    Create 02_oop.js

---
+++ 10_OOP/02_oop.js
@@ -0,0 +1,42 @@
+//Object Literals
+
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
+
+
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+//Constructor function :- new Keyword
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+//without using new the values will overwrite thus new create a seperate instance or copy to work 
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
