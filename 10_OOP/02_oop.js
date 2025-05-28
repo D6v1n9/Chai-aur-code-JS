8dfb1d899ec3b3d44ed68ea58316940dda9ed77c
Karan_d6v1n9 <132968913+D6v1n9@users.noreply.github.com>
2025-05-28 10:51:00 +0530

    Update 02_oop.js

--- 10_OOP/02_oop.js
+++ 10_OOP/02_oop.js
@@ -40,3 +40,26 @@ const userOne = new User("hitesh", 12, true)
 const userTwo = new User("ChaiAurCode", 11, false)
 console.log(userOne.constructor);
 //console.log(userTwo);
+
+// Steps of working 
+// 1. By using new keyword every time this will be called thus an empty object will be created
+// 2. Constructor function is called due to new keyword
+// 3. All the arguments get injected in the this keyword
+// 4. you will recieve 
+
+
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+}
+const auto = new Car("Honda", "Accord", 1998);
+
+//object instanceof constructor
+
+console.log(auto instanceof Car);
+// Expected output: true
+
+console.log(auto instanceof Object);
+// Expected output: true
+
