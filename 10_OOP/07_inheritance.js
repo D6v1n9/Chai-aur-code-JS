41c2555e0d8b0ab17518c756214a996e6b6b8072
Karan_d6v1n9 <132968913+D6v1n9@users.noreply.github.com>
2025-05-28 13:38:31 +0530

    Create 07_inheritance.js

---
+++ 10_OOP/07_inheritance.js
@@ -0,0 +1,32 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)                        // Behind the scene call() is going with this keyword 
+        this.email = email                    // After super you have username access ih Teacher class
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123")
+
+chai.logMe()
+const masalaChai = new User("masalaChai")
+
+masalaChai.logMe()
+
+console.log(chai === Teacher)    // false
+console.log(chai instanceof User);  // True
+console.log(chai instanceof Teacher);  // True
