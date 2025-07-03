f8cb79b79efb9db2a57c5f95eee713919411fba4
Karan_d6v1n9 <132968913+D6v1n9@users.noreply.github.com>
2025-07-03 23:09:38 +0530

    Update 02_scope.js

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -59,3 +59,33 @@ addTwo(5)
 const addTwo = function(num){
     return num + 2
 }
+
+/*    SELF LEARNING  */
+
+function outer() {
+  const msg = "Hello from outer scope";
+
+  function inner() {
+    console.log(msg); // ✅ This prints "Hello from outer scope"
+  }
+
+  inner();
+}
+
+outer();
+
+// Creating Shadow of a variable with same name
+
+function outer() {
+  const msg = "Hello from outer scope";
+
+  function inner() {
+    console.log(msg); // ❌ ReferenceError: Cannot access 'msg' before initialization
+
+    const msg = "Hello from inner scope"; // shadowed here
+  }
+
+  inner();
+}
+
+// ✅ Yes, you can access const from an outer scope inside an inner scope, unless you shadow it with a variable of the same name.
